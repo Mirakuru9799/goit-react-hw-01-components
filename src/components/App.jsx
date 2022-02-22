efc0c74e00e12f726f9af28a390820baa013cc7f
Marina Hrytsai <polanic@ukr.net>
2022-02-23 02:10:50 +0300

    add first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
-export const App = () => {
+//Задание 1. Профиль социальной сети
+import user from './Data/user.json';
+import Profile from './Profile/Profile';
+//Задание 2. Секция статистики
+//import data from './components/Statistics/data.json';
+//import Statistics from './components/Statistics/Statistics';
+// Задание 3. Список друзей
+//import FriendsList from './components/Friends/FriendsList';
+//import friends from './components/Friends/friends.json';
+// Задание 4. История транзакций
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />    
     </div>
   );
-};
+}
+
+export default App;
+//<Statistics stats={data} />
+//<FriendsList friends={friends} />
